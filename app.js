4da26561039e31c2a3f907ceea55d3c45bb5a0b9
Sed2295 <sed.silver@hotmail.com>
2020-10-30 14:45:44 -0600

    Primera version

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helper');
+
+//desplegar en heroku o local
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/views'))
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); 
+app.use('/jq', express.static(__dirname + '/node_modules/jquery/dist')); 
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/parciales')
+
+app.get('/',  (req, res) =>  {
+	//home o index pagina prinicpal
+})
+app.get('/views/emp', function (req, res) {
+  res.render('empleados');
+});
+app.get('/views/reg', function (req, res) {
+  res.render('registro_empleados');
+});
+
+app.listen(port, () => {
+	console.log(`Escuchando peticiones en el puerto ${port}`)
+})
+//http://localhost:3000/views/emp  http://localhost:3000/views/emp
